a6d6e030382fcb5cccd1aa14783a25f4dac6cceb
Swanjira <sylviahwanjira01@gmail.com>
2023-02-20 22:52:44 -0800

    Task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * main - prints alphabets ten times
+ * Return: void
+ */
+void print_alphabet_x10(void)
+{
+	char a;
+	int i = 0;
+
+	while (i <= 0)
+	{
+		for (a = 'a'; a <= 'z'; a++)
+		{
+			_putchar(a);
+			_putchar('\n');
+			i++;
+		}
+	}
+}
+
