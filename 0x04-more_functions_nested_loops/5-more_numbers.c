bf7f7a3cd2c7b60a8fb9a695ee175d34730ec89c
Swanjira <sylviahwanjira01@gmail.com>
2023-02-23 07:12:53 -0800

    Task 5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,22 +1,25 @@
 #include "main.h"
+
 /**
  * more_numbers - prints 0 - 14 ten times
- * Return: numbers 1 - 14 ten times
+ *
+ * Return: void
  */
+
 void more_numbers(void)
 {
-	int a, b;
+    int a, b;
 
-	for (a = 0; a < 10; a++)
-	{
-	for (b = 0; b <= 14; b++);
-	{
-	if (b > 9)
-	{
-	_putchar((b / 10) + '0);
-	}
-	_putchar((b % 10) + '0');
-	}
-	_putchar('\n');
-	}
+    for (a = 0; a < 10; a++)
+    {
+        for (b = 0; b <= 14; b++)
+        {
+            if (b > 9)
+            {
+                _putchar((b / 10) + '0');
+            }
+            _putchar((b % 10) + '0');
+        }
+        _putchar('\n');
+    }
 }
---
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <unistd.h>
+/**
+ * _putchar - writes the character c to stdout
+ * @c: character to be printed
+ * Return: On success 1 and -1 on error
+ */
+
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
