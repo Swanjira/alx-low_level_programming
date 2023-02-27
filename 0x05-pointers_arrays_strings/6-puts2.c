cc2377ec5e7893729986d5a12c9bf933da729ca1
Swanjira <sylviahwanjira01@gmail.com>
2023-02-27 05:57:29 -0800

    Task 6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -8,16 +8,23 @@
  */
 void puts2(char *str)
 {
-	int longi = 0
+	int longi = 0;
+	int a = 0;
+	char *b = str;
 	int c;
 
-	while (str[longi] != '\0')
+	while (*b != '\0')
 	{
+		b++;
 		longi++;
 	}
-	for (c = 0; c < longi; c += 2)
+	a = longi - 1;
+	for (c = 0; c <= a; c++)
+	{
+		if (c % 2 == 0)
 		{
 			putchar(str[c]);
 		}
+	}
 	putchar('\n');
 }
