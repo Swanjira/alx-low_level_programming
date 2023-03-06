9907eaf8d27509391c8f4e28b7be9062c5ea33fe
Swanjira <sylviahwanjira01@gmail.com>
2023-03-05 22:19:13 -0800

    Header filr and putchar

---
+++ 0x07-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * _putchar - writes char c to stdout
+ * @c: char to be printed
+ *
+ * Return: 1 on Success
+ * Return: -1 on error
+ */
+
+int _putchar(char c)
+{
+	return (write(1 &c, 1));
+}
