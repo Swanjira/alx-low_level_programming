709244ca3c583e2cbe3918e7c949af7c86d37c0e
Swanjira <sylviahwanjira01@gmail.com>
2023-03-07 22:20:27 -0800

    Header file

---
+++ 0x08-recursion/_putchar.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+
+/**
+ * _putchar - writes the char c to stdout
+ * @c: char to be printed
+ *
+ * Return: 1 on Success -1 on error
+ */
+
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
