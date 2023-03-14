5737a2415fc2206f68f2814e509111e8738a05e5
Swanjira <sylviahwanjira01@gmail.com>
2023-03-13 22:00:52 -0700

    Header file, readme and putchar

---
+++ 0x0B-malloc_free/_putchar.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * _putchar - writes char c to stdout
+ * @c: char to print
+ *
+ * Return 1 on success -1 on error
+ */
+
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
