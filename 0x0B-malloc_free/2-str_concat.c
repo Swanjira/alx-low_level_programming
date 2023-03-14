cf047ee63c5e17363276f24ec8e702b84bd3f6ac
Swanjira <sylviahwanjira01@gmail.com>
2023-03-13 23:19:56 -0700

    Task 2

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,49 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * str_concat - concatenates two strings
+ * @s1: imput
+ * @s2: input
+ *
+ * Return: concatenation of s1 and s2 if successful
+ * NULL if failed
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	char *conct;
+
+	int x, y;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	x = y = 0;
+	while (s1[x] != '\0')
+		x++;
+	while (s2[y] != '\0')
+		y++;
+	conct = malloc (sizeof(char) * (x + y + 1));
+	if (conct == NULL)
+		return (NULL);
+	x = y = 0;
+	while (s1[x] != '\0')
+	{
+		conct[x] = s1[x];
+		x++;
+	}
+	while (s2[y] != '\0')
+	{
+		conct[x] = s2[y];
+	}
+	while (s2[y] != '\0')
+	{
+		conct[x] = s2[y];
+		x++;
+		y++;
+	}
+	conct[x] = '\0';
+	return (conct);
+}
