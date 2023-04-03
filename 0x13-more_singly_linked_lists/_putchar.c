28cc7e9e8e10751cd23905d4fd9eb16ac63eb413
Swanjira <sylviahwanjira01@gmail.com>
2023-04-02 23:29:19 -0700

    README file, putchar, header file and test files

---
+++ 0x13-more_singly_linked_lists/_putchar.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+
+/**
+ * _putchar - writes char c to stdout
+ * @c: char to be printed
+ *
+ * Return: 1 on success -1 on failure
+ */
+
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
